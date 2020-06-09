dd289415f3a0e3fe0c7e8410ae1370467cdd000c
LeopoldFrilot <lfrilo1@lsu.edu>
2020-06-08 19:40:06 -0500

    Implemented preliminary camera follow

---
+++ Assets/Scripts/Level/CameraFollow.cs
@@ -0,0 +1,36 @@
+﻿using FightingGame.Core;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FightingGame.Level
+{
+    public class CameraFollow : MonoBehaviour
+    {
+        PlayerSelect PS;
+        FrameTest FT;
+        [SerializeField] float cameraMoveSpeed = 5f;
+        float step;
+        public void Start()
+        {
+            PS = FindObjectOfType<PlayerSelect>();
+            FT = FindObjectOfType<FrameTest>();
+        }
+        public void Update()
+        {
+            HorizontalFollow();
+        }
+
+        private void HorizontalFollow()
+        {
+            float middle = (PS.Player1.transform.position.x + PS.Player2.transform.position.x) / 2f;
+            step = cameraMoveSpeed * FT.CurFrameTime;
+            Camera.main.transform.position = Vector3.MoveTowards(
+                Camera.main.transform.position,
+                new Vector3(middle, Camera.main.transform.position.y, Camera.main.transform.position.z),
+                step);
+        }
+    }
+}
+
---
+++ Assets/Scripts/PlayerSelect.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FightingGame
+{
+    public class PlayerSelect : MonoBehaviour
+    {
+        [SerializeField] GameObject player1;
+        [SerializeField] GameObject player2;
+
+        public GameObject Player1 { get => player1; private set => player1 = value; }
+        public GameObject Player2 { get => player2; private set => player2 = value; }
+    }
+}
+
